1b587b1560a79091bd2961612699334dd19afa10
aLeuleu <97832618+aLeuleu@users.noreply.github.com>
2023-09-21 16:00:24 +0200

    SAVE 2

--- srcs/requirements/wordpress/tools/wp_install.sh
+++ srcs/requirements/wordpress/tools/wp_install.sh
@@ -14,18 +14,18 @@ echo "config create..."
                                   --path=$WP_PATH
 
 echo "core install..."
-/var/wp-cli.phar core install   --title=$WP_TITLE \
-                                --url=$WP_URL \
-                                --admin_user=$WP_ADMIN_USER \
-                                --admin_email=$WP_ADMIN_EMAIL \
-                                --path=$WP_PATH \
-                                --skip-email \
-                                --allow-root
+./var/wp-cli.phar core install   --url="${WP_URL}" \
+                  --title="${WP_TITLE}" \
+                  --admin_user="${WP_ADMIN_USER}" \
+                  --admin_password="${WP_ADMIN_PASSWORD}" \
+                  --admin_email="${WP_ADMIN_EMAIL}" \
+                  --allow-root \
+                  --path=${WP_PATH}
 
 echo "user create..."
-/var/wp-cli.phar user create $WP_USER $WP_EMAIL \
-                             --path=$WP_PATH \
-                             --allow-root
+/var/wp-cli.phar user create $WP_USER $WP_USER_EMAIL \
+                               --allow-root \
+                               --path=$WP_PATH
 fi
 mkdir -p /run/php
 /usr/sbin/php-fpm7.3 -F # -F = stay in foreground mode (don't daemonize)
